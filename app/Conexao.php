7c07950b4776e1fc7f8afea0e03e7c75bb65690c
Gledson Ximenes <gledsoneadufgd@gmail.com>
2017-07-22 18:10:01 -0400

    Comentado

--- app/Conexao.php
+++ app/Conexao.php
@@ -3,10 +3,10 @@
 /*
  * Estabelece a conexão com banco de dados MySQL!
  */
-$servidor = 'localhost';
-$usuario = 'root';
-$senha = '';
-$database = 'cfcsystem';
+$servidor = 'localhost'; // Inseri host
+$usuario = 'root'; // Inseri usuário
+$senha = ''; // Inseri senha
+$database = 'cfcsystem'; // Inseri banco de dados
 
 function conectar($servidor, $usuario, $senha, $database) {
     $conn = mysqli_connect($servidor, $usuario, $senha) or die("MySQL: Não foi possível conectar-se ao servidor [" . $servidor . "].");
